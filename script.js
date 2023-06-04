0654539cdd7328d495c1058d51afbb35b6435ca0
Ajay <ajayzyan100@gmail.com>
2023-06-04 11:52:01 +0530

    resued setattibute funtion created

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const count = 10;
 const apiKey = 'q99h7oGVdlvh6EM0nSQdLw2w1VXHVm57kaqO3asQa9g';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+//helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 
 //create elements for links & photos, add to DOM
 function displayPhotos() {
@@ -15,14 +21,18 @@ function displayPhotos() {
   photosArray.forEach((photo) => {
     //create <a>
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
 
     //create <img>
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    setAttributes(item, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
     //put <img> inside <a>,then put both inside img container element
     item.appendChild(img);
